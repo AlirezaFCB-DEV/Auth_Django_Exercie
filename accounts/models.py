0e3df8f6bdf44a8d9651d01e6632ad3920f7632e
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-10-19 17:02:55 +0330

    feat : editing User model in django with abstract base user

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,47 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , PermissionsMixin , BaseUserManager
+from django.utils import timezone
 
 # Create your models here.
 
-#! If you just want to add a few fields, or slightly modify existing fields, use AbstractUser.
-class User (AbstractUser) :
-    """
-    User model
-    """
-    phone = models.CharField(max_length=20 , blank=True , null=True)
-    avatar = models.ImageField(upload_to="avatars/" , blank=True , null=True)
+#! If you want to have full control over the user (e.g. remove the username and use only the email), use AbstractBaseUser. This method is more complex but is useful in the marketplace for specific cases.
+
+class Custom_User_Manager(BaseUserManager):
+    def create_user(self , email , password=None , **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email , **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        
+        return user
+    
+    def create_superuser(self , email , password=None , **extra_fields):
+        extra_fields.setdefault("is_staff" , True)
+        extra_fields.setdefault("is_superuser" , True)
+        extra_fields.setdefault("is_active" , True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("SuperUser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("SuperUser must have is_superuser=True")
+        
+        return self.create_user(email=email , password=password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30 , blank=True)
+    last_name = models.CharField(max_length=30 , blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    
+    objects = Custom_User_Manager()
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
     
     def __str__(self):
-        return super().username
+        return self.email
+    
