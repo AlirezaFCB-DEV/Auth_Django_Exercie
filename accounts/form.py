6ad9e0588e3fa0a95ef2679ed79462451d5213a6
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-10-21 14:06:16 +0330

    feat :  register form

---
+++ accounts/form.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+class RegisterFrom(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ("email" , "password1" , "password2")
+        
