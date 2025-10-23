d38eca5d4f7771028dc0432c4e0dd90b85b9f122
AlirezaFCB-DEV <alirezadevloperui@gmail.com>
2025-10-23 11:57:42 +0330

    feat: coding register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .form import RegisterFrom
 # Create your views here.
+
+
+def register_view(req):
+    if req.method == "POST":
+        form = RegisterFrom(req.POST, req.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(req, user)
+            return redirect("accounts:profile")
+    else:
+        form = RegisterFrom()
+        return render(req, "accounts/register.html", {"form": form})
+
